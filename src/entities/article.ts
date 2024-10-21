6a22d5508812c57fbd0c48f2aece24731c8e0965
Fabian Loewe <fabianloewe97@gmail.com>
2024-10-21 15:44:50 +0200

    Added default values for Articles and minor version bump
    Without the default values, the automatic creation of the fields query for the API doesn't work because the fields would be undefined and thus not recognized as Object properties.

--- src/entities/article.ts
+++ src/entities/article.ts
@@ -1,101 +1,107 @@
-import {User} from "./user";
-import {Project} from "./project";
-import {Visibility} from "./visibility";
+import {ReducedUser, ReducedUserImpl} from "./user";
+import {ReducedProject, ReducedProjectImpl} from "./project";
+import {LimitedVisibilityImpl, Visibility} from "./visibility";
 import {Issue} from "./issue";
 
-export class BaseArticleImpl {
-    id?: string;
-    attachments?: ArticleAttachment[];
-    content?: string;
-    reporter?: User;
-    summary?: string;
-    visibility?: Visibility;
+export class ReducedBaseArticleImpl {
+    id?: string = '';
+    summary?: string = '';
+    visibility?: Visibility = new LimitedVisibilityImpl();
+}
+
+export interface ReducedBaseArticle extends ReducedBaseArticleImpl {
+}
+
+export class BaseArticleImpl extends ReducedBaseArticleImpl {
+    attachments?: ArticleAttachment[] = [];
+    content?: string = '';
+    reporter?: ReducedUser = new ReducedUserImpl();
 }
 
 export interface BaseArticle extends BaseArticleImpl {
 }
 
 export class ArticleImpl extends BaseArticleImpl {
-    childArticles?: Article[];
-    comments?: ArticleComment[];
-    created?: number;
-    externalArticle?: ExternalArticle;
-    hasChildren?: boolean;
-    hasStar?: boolean;
-    idReadable?: string;
-    ordinal?: number;
-    parentArticle?: Article;
-    pinnedComments?: ArticleComment[];
-    project?: Project;
-    tags?: ArticleTag[];
-    updated?: number;
-    updatedBy?: User;
+    childArticles?: Article[] = [];
+    comments?: ArticleComment[] = [];
+    created?: number = 0;
+    externalArticle?: ExternalArticle = new ExternalArticleImpl();
+    hasChildren?: boolean = false;
+    hasStar?: boolean = false;
+    idReadable?: string = '';
+    ordinal?: number = 0;
+    parentArticle?: ReducedBaseArticle = new ReducedBaseArticleImpl();
+    pinnedComments?: ArticleComment[] = [];
+    project?: ReducedProject = new ReducedProjectImpl();
+    tags?: ArticleTag[] = [];
+    updated?: number = 0;
+    updatedBy?: ReducedUser = new ReducedUserImpl();
 }
 
 export interface Article extends ArticleImpl {
 }
 
 export class ExternalArticleImpl {
-    id?: string;
-    name?: string;
-    url?: string;
-    key?: string;
+    id?: string = '';
+    name?: string = '';
+    url?: string = '';
+    key?: string = '';
 }
 
 export interface ExternalArticle extends ExternalArticleImpl {
 }
 
 export class ArticleAttachmentImpl {
-    id?: string;
-    name?: string;
-    author?: User;
-    created?: number;
-    updated?: number;
-    size?: number;
-    extension?: string;
-    charset?: string;
-    mimeType?: string;
-    metaData?: string;
-    draft?: boolean;
-    removed?: boolean;
-    base64Content?: string;
-    url?: string;
-    visibility?: Visibility;
-    article?: BaseArticle;
-    comment?: ArticleComment;
+    id?: string = '';
+    name?: string = '';
+    author?: ReducedUser = new ReducedUserImpl();
+    created?: number = 0;
+    updated?: number = 0;
+    size?: number = 0;
+    extension?: string = '';
+    charset?: string = '';
+    mimeType?: string = '';
+    metaData?: string = '';
+    draft?: boolean = false;
+    removed?: boolean = false;
+    base64Content?: string = '';
+    url?: string = '';
+    visibility?: Visibility = new LimitedVisibilityImpl();
+    article?: BaseArticle = new BaseArticleImpl();
+    comment?: ArticleComment = new ArticleCommentImpl();
 }
 
 export interface ArticleAttachment extends ArticleAttachmentImpl {
 }
 
 export class ArticleCommentImpl {
-    id?: string;
-    article?: Article;
-    attachments?: ArticleAttachment[];
-    author?: User;
-    created?: number;
-    pinned?: boolean;
-    reactions?: unknown[];
-    text?: string;
-    updated?: number;
-    visibility?: Visibility;
+    id?: string = '';
+    article?: Article = new ArticleImpl();
+    attachments?: ArticleAttachment[] = [];
+    author?: ReducedUser = new ReducedUserImpl();
+    created?: number = 0;
+    pinned?: boolean = false;
+    reactions?: unknown[] = [];
+    text?: string = '';
+    updated?: number = 0;
+    visibility?: Visibility = new LimitedVisibilityImpl();
 }
 
 export interface ArticleComment extends ArticleCommentImpl {
 }
 
 export class ArticleTagImpl {
-    id?: string;
-    issues?: Issue[];
-    color?: unknown;
-    untagOnResolve?: boolean;
-    visibleFor?: unknown;
-    updateableBy?: unknown;
-    readSharingSettings?: unknown;
-    tagSharingSettings?: unknown;
-    updateSharingSettings?: unknown;
-    owner?: User;
-    name?: string;
+    id?: string = '';
+    issues?: Issue[] = [];
+    color?: unknown = '';
+    untagOnResolve?: boolean = false;
+    visibleFor?: unknown = '';
+    updateableBy?: unknown = '';
+    readSharingSettings?: unknown = '';
+    tagSharingSettings?: unknown = '';
+    updateSharingSettings?: unknown = '';
+    owner?: ReducedUser = new ReducedUserImpl();
+    name?: string = '';
 }
 
 export interface ArticleTag extends ArticleTagImpl {
